19d15f78eadfabaa2b5a2415c43eb9de8229fe60
carefreebee <leonelchristiebaclayon@gmail.com>
2024-10-13 20:15:19 +0800

    fix

--- backend/campuseats/src/main/java/com/capstone/campuseats/config/WebConfig.java
+++ backend/campuseats/src/main/java/com/capstone/campuseats/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.capstone.campuseats.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 

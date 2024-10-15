3cb55568528af108093bd6820e301c82608e5c0d
Liden <lninyow@gmail.com>
2024-10-16 02:00:46 +0800

    wip shop analytics

--- frontend/src/components/AdminAnalytics.jsx
+++ frontend/src/components/AdminAnalytics.jsx
@@ -14,10 +14,275 @@ import axios from "../utils/axiosConfig";
 
 
 const ShopAnalytics = () => {
+    const [cancelledOrders, setCancelledOrders] = useState(0);
+    const[completedOrders, setCompletedOrders] = useState(0);
+    const [pendingShops, setPendingShops] = useState([]);
+    const [currentShops, setCurrentShops] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const[allOrders, setAllOrders] = useState([]);
+    const [selectedYear,setSelectedYear] = useState(2024)
+    const [averageOrderValue, setAverageOrderValue] = useState(0);
+ 
+
+     const fetchShops = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get('/shops/pending-lists');
+                const { pendingShops, nonPendingShops } = response.data;
+                setPendingShops(pendingShops);
+                setCurrentShops(nonPendingShops);
+            } catch (error) {
+                console.error('Error fetching shops:', error);
+            }finally {
+                setLoading(false);
+            }
+        };
+
+
+    const calculateAverageOrder = (orders) => {
+      const totalValue = orders.reduce((acc, order) => acc + order.totalPrice, 0);
+      const averageValue = totalValue / orders.length;
+
+      return averageValue.toFixed(2);
+
+    }
+        
+
+    const fetchOrders = async () => {
+        setLoading(true);
+        try{
+            const response = await axios.get('/orders/completed-orders')
+            const allOrders = response.data.completedOrders;
+            setAllOrders(allOrders);
+            console.log(allOrders)
+        
+        const maonani = allOrders.filter(order => order.status === 'completed')
+        const shopOrderCounts = maonani.reduce((acc, order) => {
+            const shopId = order.shopId;
+            if(!acc[shopId]){
+                acc[shopId] = 0;
+            }
+            acc[shopId]++;
+            return acc;
+        },{});
+        
+      console.log(shopOrderCounts);
+         const completedOrders = allOrders.filter(order => order.status === 'completed').length;
+      const cancelledByShop = allOrders.filter(order => order.status === 'cancelled_by_shop').length;
+      const cancelledByCustomer = allOrders.filter(order => order.status === 'cancelled_by_customer').length;
+      const cancelledByDasher = allOrders.filter(order => order.status === 'cancelled_by_dasher').length;
+      const noShow = allOrders.filter(order => order.status === 'no-show').length;
+      const totalOrders = completedOrders + cancelledByShop + cancelledByCustomer + cancelledByDasher + noShow;
+      const completedPercentage = (completedOrders / totalOrders) * 100;
+      const cancelledPercentage = ((cancelledByShop + cancelledByCustomer + cancelledByDasher + noShow) / totalOrders) * 100;
+      const averageOrderValue = calculateAverageOrder(allOrders);
+
+      setAverageOrderValue(averageOrderValue);
+      setCompletedOrders(completedPercentage.toFixed(2));
+      setCancelledOrders(cancelledPercentage.toFixed(2));
+
+
+
+
+ setCurrentShops((prevShops) =>
+        prevShops.map((shop) => ({
+          ...shop,
+          completedOrders: shopOrderCounts[shop.id] || 0,
+        })).sort((a, b) => b.completedOrders - a.completedOrders));
+
+        }catch(error){
+            console.error('Error fetching orders:', error.response.data.error);
+        }finally{
+            setLoading(false);
+        }
+    }
+
+
+  const formatCompletedOrdersByMonth = (orders, selectedYear) => {
+  
+  const monthNames = [
+    "Jan", "Feb", "March", "April", "May", "June",
+    "July", "Aug", "Sept", "Oct", "Nov", "Dec"
+  ];
+
+  const ordersByMonth = {
+    completed: { "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0, "12": 0 },
+    cancelled: { "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0, "12": 0 }
+  };
+
+  orders.forEach(order => {
+    const orderDate = new Date(order.createdAt);
+    const month = orderDate.getMonth() + 1;
+
+    if (orderDate.getFullYear() === selectedYear) {
+     if (["completed","cancelled_by_customer", "cancelled_by_shop", "cancelled_by_dasher","no-show"].includes(order.status)) {
+        ordersByMonth.completed[month]++;
+      }
+    }
+  });
+
+
+  const xAxisData = monthNames; 
+  const yAxisCompleted = Object.values(ordersByMonth.completed); 
+
+  return { xAxisData, yAxisCompleted };
+};
+
+
+const { xAxisData, yAxisCompleted } = formatCompletedOrdersByMonth(allOrders,selectedYear);
+
+
+
+const handleYearChange = (event) => {
+    setSelectedYear(event.target.value);
+  };
+
+     useEffect(() => {
+  const fetchData = async () => {
+    await fetchShops();
+    await fetchOrders();
+  };
+  fetchData();
+
+}, []);
+
+
   return (
-    <div className="p-4 items-center justify-center w-full h-full flex flex-col">
-      <h2>Shop Analytics</h2>
-      <p>Details about Shop analytics go heredsadasdsa.</p>
+    <div className="p-1 items-center justify-center w-full h-full flex flex-col gap-2">
+      <div className='flex items-center justify-between w-full gap-8'>
+        <div className=' w-[500px] h-[550px] shadow-2xl rounded-2xl p-4 overflow-auto hover:scale-[1.01] transition-transform duration-300'>
+        <div className='flex w-full justify-between items-center'>
+            <h2 className='font-semibold'>Top Performing Shops</h2>
+            <h2 className='font-semibold'>Orders Completed</h2>
+        </div>
+       {loading ? (<div className="flex justify-center items-center h-full w-full">
+                        <div
+                            className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                            role="status">
+                            <span
+                                className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                            >Loading...</span>
+                        </div>
+                    </div>) : currentShops.map((shop, index) => (
+          <div key={shop.id} className="adl-box p-2 rounded-lg overflow-auto">
+            <div className="adl-box-content">
+              <div className="flex items-center gap-2">
+                <span>{index + 1}.</span> 
+                <img src={shop.imageUrl} alt="Shop profile" className="w-16 h-16" />
+              </div>
+              <div className='w-5'>{shop.name}</div>
+              <div>{shop.completedOrders}</div>
+            </div>
+          </div>
+        ))}
+        </div>
+         <div className='flex flex-col gap-8'>
+            <div className='items-center justify-center flex flex-col border  w-[300px] h-[250px] shadow-2xl rounded-2xl p-4 hover:scale-[1.02] transition-transform duration-300'>
+           <h2 className='text-xl font-semibold self-start '>Total Handled Orders</h2> 
+           {loading ? (<div className="flex justify-center items-center h-full w-full">
+                        <div
+                            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                            role="status">
+                            <span
+                                className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                            >Loading...</span>
+                        </div>
+                    </div>) : <div className='h-full text-[128px]'>{allOrders.length}</div>}
+        
+        </div>
+           <div className='items-center justify-center flex flex-col border  w-[300px] h-[250px] shadow-2xl rounded-2xl p-4 hover:scale-[1.02] transition-transform duration-300'>
+           <h2 className='text-xl font-semibold self-start '>Avg. Order Value</h2> 
+           {loading ? (<div className="flex justify-center items-center h-full w-full">
+                        <div
+                            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                            role="status">
+                            <span
+                                className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                            >Loading...</span>
+                        </div>
+                    </div>) : <div className='h-full text-[92px] items-center justify-center flex flex-col'>
+                      <div>{averageOrderValue}₱</div>
+                      </div>}
+        
+        </div>
+        </div>
+        <div className='flex flex-col items-start gap-2'>
+          <div className=' w-[800px] h-[200px] hover:scale-[1.01] transition-transform duration-300 shadow-2xl rounded-2xl p-4 flex flex-col items-center justify-center'>
+           TBA NEED IDEA
+          </div>
+        <div className=' w-[800px] h-[300px] hover:scale-[1.01] transition-transform duration-300 shadow-2xl rounded-2xl p-4 flex flex-col items-center justify-center'>
+        <div className='flex items-center justify-between w-full'>
+            <h2 className='font-semibold'>Orders Overtime</h2>
+            <div className='w-[100px]'>
+                 <FormControl fullWidth>
+      <InputLabel id="year-select-label">Year</InputLabel>
+      <Select
+        labelId="year-select-label"
+        id="year-select"
+        value={selectedYear}
+        label="year"
+        onChange={handleYearChange}
+      >
+       {[2023, 2024, 2025, 2026, 2027, 2028].map(year => (
+          <MenuItem key={year} value={year}>{year}</MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+                </div>
+            </div>
+            {  loading ? (<div className="flex justify-center items-center h-full w-full">
+                        <div
+                            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                            role="status">
+                            <span
+                                className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                            >Loading...</span>
+                        </div>
+                    </div>):  
+           <LineChart
+          xAxis={[{ data: xAxisData, label:'Month',scaleType: 'band' }]}
+           series={[
+                    {
+                      data: yAxisCompleted, 
+                      label: 'Handled Orders',
+                      color: 'green',
+                    },
+                  ]}
+                  width={1000}
+                  height={270}
+              />
+              }
+       
+        </div>
+        </div>
+      </div>
+      <div className='w-full h-[200px] hover:scale-[1.01] transition-transform duration-300 shadow-2xl rounded-2xl p-4 overflow-auto'>
+        <h2 className='font-semibold'>NEED IDEA TBA</h2>
+         {loading ? (<div className="flex justify-center items-center h-full w-full">
+                        <div
+                            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                            role="status">
+                            <span
+                                className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                            >Loading...</span>
+                        </div>
+                    </div>): 
+                    // allDashers.map(dasher => (
+                    //             <div key={dasher.id} className="adl-box">
+                    //                 <div className="adl-box-content">
+                    //                     <div>{dasher.userData.firstname + " " + dasher.userData.lastname}</div>
+                    //                     <div>{dasher.daysAvailable.join(', ')}</div>
+                    //                     <div>{dasher.availableStartTime}</div>
+                    //                     <div>{dasher.availableEndTime}</div>
+                    //                     <div>
+                    //                         <img src={dasher.schoolId} alt="School ID" className="adl-list-pic" />
+                    //                     </div>
+                    //                     <div>{dasher.status}</div>
+                    //                 </div>
+                    //             </div>
+                    //         ))
+                            <div></div>}
+      </div>
     </div>
   );
 }
@@ -148,13 +413,11 @@ const { xAxisData, yAxisCompleted, yAxisCancelled } = formatCompletedOrdersByMon
 
 const sampleData = [
   {
-    label: 'Completed Orders',
     value: completedOrders,
     color: 'green',
     
   },
   {
-    label: 'Cancelled Orders',
     value: cancelledOrders,
     color: 'red',
   },
@@ -179,7 +442,7 @@ const handleYearChange = (event) => {
   return (
     <div className="p-2 items-center justify-center w-full h-full flex flex-col gap-2">
       <div className='flex items-center justify-between w-full gap-8'>
-        <div className=' w-[450px] h-[550px] shadow-2xl rounded-2xl p-4 overflow-auto hover:scale-[1.01] transition-transform duration-300'>
+        <div className=' w-[550px] h-[550px] shadow-2xl rounded-2xl p-4 overflow-auto hover:scale-[1.01] transition-transform duration-300'>
         <div className='flex w-full justify-between items-center'>
             <h2 className='font-semibold'>Top Dashers</h2>
             <h2 className='font-semibold'>Total Orders Completed</h2>
@@ -206,8 +469,18 @@ const handleYearChange = (event) => {
         ))}
         </div>
          <div className='flex flex-col gap-8'>
-            <div className='items-center justify-center flex flex-col border  w-[600px] h-[550px] shadow-2xl rounded-2xl p-4 hover:scale-[1.02] transition-transform duration-300'>
-           <h2 className='text-2xl font-semibold self-start '>Completed Orders vs Cancelled Orders</h2> 
+            <div className='items-center justify-center flex flex-col border  w-[450px] h-[550px] shadow-2xl rounded-2xl p-4 hover:scale-[1.02] transition-transform duration-300'>
+           <h2 className='text-xl font-semibold self-start '>Completed Orders vs Cancelled Orders</h2> 
+           <div className='self-end mt-6 flex-col flex items-start'>
+            <div className='flex flex-row items-center justify-center gap-2'>
+            <div className='rounded-full bg-green-700 w-4 h-4'></div>
+            <div>Completed Orders</div>
+            </div>
+             <div className='flex flex-row items-center justify-center gap-2'>
+            <div className='rounded-full bg-red-700 w-4 h-4'></div>
+            <div>Cancelled Orders</div>
+            </div>
+           </div>
            {loading ? (<div className="flex justify-center items-center h-full w-full">
                         <div
                             className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
@@ -217,7 +490,6 @@ const handleYearChange = (event) => {
                             >Loading...</span>
                         </div>
                     </div>) : 
-                    // <h2 className="text-[72px] h-full mt-6">{cancelledOrders}</h2>}
         <PieChart
   series={[
     {
@@ -226,19 +498,18 @@ const handleYearChange = (event) => {
       //   { id: 1, value: cancelledOrders, label: `Cancelled Orders`, color:'red' },
       // ],
       data: sampleData,
-     faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+     faded: { innerRadius: 20, additionalRadius: -20, color: 'gray' },
     highlightScope: { fade: 'global', highlight: 'item' },
     arcLabel: 'value',
     valueFormatter,
-    legend: { direction: 'row', position: { vertical: 'middle', horizontal: 'top' }, padding: 30 },
     },
   ]}
-  height={300}
-  width={700}
-/>   }
+  height={400}
+  width={400}
+/>  }
         </div>
         </div>
-        <div className=' w-[800px] h-[550px] hover:scale-[1.01] transition-transform duration-300 shadow-2xl rounded-2xl p-4 flex flex-col items-center justify-center'>
+        <div className=' w-[750px] h-[550px] hover:scale-[1.01] transition-transform duration-300 shadow-2xl rounded-2xl p-4 flex flex-col items-center justify-center'>
         <div className='flex items-center justify-between w-full'>
             <h2 className='font-semibold'>Orders Overtime</h2>
             <div className='w-[100px]'>
@@ -346,7 +617,7 @@ const color = red[400];
         <Tab value="3" label="Shop" sx={{fontWeight:'bold'}} />
       </Tabs>
          </div>
-    <div className="w-full h-[90%] rounded-b-lg border bg-[#FFFAF1]">
+    <div className="w-full h-full rounded-b-lg border bg-[#FFFAF1]">
   <TabPanel value="2">
            <DashersAnalytics/>
           </TabPanel>
